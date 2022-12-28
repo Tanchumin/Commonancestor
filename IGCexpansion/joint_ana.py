bbbdf98e88ab2a4a67a096fb9a0b14b2442d30d4
Tanchumin Xu <txu7@Tanchumins-MBP.attlocal.net>
2022-12-28 13:05:00 -0500

    convey parameters

--- IGCexpansion/joint_ana.py
+++ IGCexpansion/joint_ana.py
@@ -520,7 +520,7 @@ class JointAnalysis:
 #        print(self.get_Hessian())
 
 
-    def em_joint_hessian(self,epis=0.1,MAX=3):
+    def em_joint_hessian(self,epis=0.1,MAX=3,med=7,scaletau=40,scalek=25/4):
         for ii in range(len(self.paralog_list)):
             self.geneconv_list[ii].id = self.geneconv_list[ii].compute_paralog_id()
             print(self.geneconv_list[ii].id, flush=True)
@@ -545,7 +545,7 @@ class JointAnalysis:
 
         print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
       #  print(self.get_Hessian())
-        self.surface()
+        self.surface(med=med,scaletau=scaletau,scalek=scalek)
 
 
     def surface(self,med=7,scaletau=40,scalek=25/4):
